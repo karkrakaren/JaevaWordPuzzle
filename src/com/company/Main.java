bc6ab0ab8e45bb50a656271ec4163149176b66d8
Karen <karenannebeckers@gmail.com>
2016-03-11 11:53:39 +0100

    dit en dit geadd

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+
 public class Main {
 
     public static void main(String[] args) {
+        private static String[] readfile("6x4_1.txt") {
+            try {
+                String letters = new BufferedReader(new FileReader("6x4_1.txt"));
+            }
+            catch(Exception e) {
+                System.out.println("Error.");
+            }
 	    ChooseWord.makeBoard();
         System.out.println("je Moeder");
     }
